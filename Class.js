196726950bbb7f002b2608953f40790e6a5ee69a
leoaee <leoaee@gmail.com>
2014-03-27 23:30:58 +0800

    instence-methods

--- Class.js
+++ Class.js
@@ -1,5 +1,9 @@
 module.exports = Class = function (obj) {
     if (obj.initialize) {
+        for (var i in obj) {
+        if(i != 'initialize'){
+        obj.initialize.prototype[i] = obj[i];
+    }}
         return obj.initialize;
     } else {
         return function () {
