afca96b1358fd526b80a70567f1efaa9037b7995
leoaee <leoaee@gmail.com>
2014-03-29 16:44:53 +0800

    methods-inheritance

--- Class.js
+++ Class.js
@@ -1,3 +1,4 @@
+/**
 module.exports = Class = function (obj) {
     if (obj.initialize) {
         for (var i in obj) {
@@ -10,3 +11,40 @@ module.exports = Class = function (obj) {
         };
     }
 };
+*/
+
+module.exports = Class = function (sub,sup) {
+  if (sup) {
+    sub.ctr = sup;
+    for (var key in sup.prototype) {
+        sub.ctr.prototype[key] = sup.prototype[key];
+    }
+    for (var key in sub) {
+      if(key != 'ctr'){
+        sub.ctr.prototype[key] = sub[key];
+      }
+    }
+
+    return sub.ctr;
+  } else {
+    if (sub.initialize) {
+    for (var i in sub) {
+        if (i != 'initialize') {
+          sub.initialize.prototype[i] = sub[i];
+
+        }
+      }
+
+        return sub.initialize;
+    } else {
+      sub.ctr = function () {};
+      for (var i in sub){
+        if(i != 'ctr'){
+        sub.ctr.prototype[i] = sub[i];
+        }
+      }
+          return sub.ctr;
+        }
+      }
+
+}
