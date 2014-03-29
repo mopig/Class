cb252ffaa0ac54638b6fa976068532a6d298f470
leoaee <leoaee@gmail.com>
2014-03-30 01:24:39 +0800

    super-class

--- Class.js
+++ Class.js
@@ -1,50 +1,34 @@
-/**
-module.exports = Class = function (obj) {
-    if (obj.initialize) {
-        for (var i in obj) {
-        if(i != 'initialize'){
-        obj.initialize.prototype[i] = obj[i];
-    }}
-        return obj.initialize;
+module.exports = Class = function (sub, sup) {
+    if (sup) {
+        sub.ctr = sup;
+        for (var key in sup.prototype) {
+            sub.ctr.prototype[key] = sup.prototype[key];
+        }
+        for (var key in sub) {
+            if(key != 'ctr'){
+            sub.ctr.prototype[key] = sub[key];
+            }
+        }
+        sub.ctr.__super__ = sup;
+        return sub.ctr;
     } else {
-        return function () {
-        };
-    }
-};
-*/
-
-module.exports = Class = function (sub,sup) {
-  if (sup) {
-    sub.ctr = sup;
-    for (var key in sup.prototype) {
-        sub.ctr.prototype[key] = sup.prototype[key];
-    }
-    for (var key in sub) {
-      if(key != 'ctr'){
-        sub.ctr.prototype[key] = sub[key];
-      }
-    }
-
-    return sub.ctr;
-  } else {
-    if (sub.initialize) {
-    for (var i in sub) {
-        if (i != 'initialize') {
-          sub.initialize.prototype[i] = sub[i];
-
+        if (sub.initialize) {
+            for (var i in sub) {
+                if (i != 'initialize') {
+                    sub.initialize.prototype[i] = sub[i];
+                }
+            }
+            sub.initialize.__super__ = Object;
+            return sub.initialize;
+        } else {
+            sub.ctr = function () {};
+            for (var i in sub){
+                if(i != 'ctr'){
+                    sub.ctr.prototype[i] = sub[i];
+                }
+            }
+            sub.ctr.__super__ = Object;
+            return sub.ctr;
         }
-      }
-
-        return sub.initialize;
-    } else {
-      sub.ctr = function () {};
-      for (var i in sub){
-        if(i != 'ctr'){
-        sub.ctr.prototype[i] = sub[i];
-        }
-      }
-          return sub.ctr;
-        }
-      }
-
+    }
 }
