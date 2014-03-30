bd609856bf5116d7239bac53caa13d25a6711845
leoaee <leoaee@gmail.com>
2014-03-30 21:54:02 +0800

    modify Class

--- Class.js
+++ Class.js
@@ -1,34 +1,23 @@
-module.exports = Class = function (sub, sup) {
+module.exports = function(sub, sup) {
+    var cstor = sub.initialize || function() {};
+    cstor.prototype.__super__ = sup || Object;
+
     if (sup) {
-        sub.ctr = sup;
-        for (var key in sup.prototype) {
-            sub.ctr.prototype[key] = sup.prototype[key];
+        for (var key in sub){
+            sup.prototype[key] = sub[key];
         }
-        for (var key in sub) {
-            if(key != 'ctr'){
-            sub.ctr.prototype[key] = sub[key];
-            }
-        }
-        sub.ctr.__super__ = sup;
-        return sub.ctr;
+        cstor.prototype = new sup();
     } else {
-        if (sub.initialize) {
-            for (var i in sub) {
-                if (i != 'initialize') {
-                    sub.initialize.prototype[i] = sub[i];
-                }
-            }
-            sub.initialize.__super__ = Object;
-            return sub.initialize;
-        } else {
-            sub.ctr = function () {};
-            for (var i in sub){
-                if(i != 'ctr'){
-                    sub.ctr.prototype[i] = sub[i];
-                }
-            }
-            sub.ctr.__super__ = Object;
-            return sub.ctr;
+        for (var key in sub){
+            cstor.prototype[key] = sub[key];
         }
     }
-}
+    
+    cstor.prototype.super = function (name) {
+        var args = arguments;
+        return this.__super__.prototype[name]
+        ?this.__super__.prototype[name].apply(this, [].slice.call(args, 1))
+        :this.super.apply(this,args);
+    }
+    return cstor;
+};
